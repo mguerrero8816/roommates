8273c41e765e73579d301d2befcb1523e71b78ef
Mike Guerrero <mguerrero@ceatus.com>
2016-12-24 23:33:34 -0800

    added users to transactions

--- db/migrate/20161125031149_create_transactions.rb
+++ db/migrate/20161125031149_create_transactions.rb
@@ -1,11 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
-      t.user :references
       t.integer :cents
       t.integer :split
       t.datetime :due
       t.datetime :paid
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125004603) do
+ActiveRecord::Schema.define(version: 20161125031149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "cents"
+    t.integer  "split"
+    t.datetime "due"
+    t.datetime "paid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
