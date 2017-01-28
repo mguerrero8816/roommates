260560e323fc13465c2c4030f004cdddd7c6f981
Mike Guerrero <mguerrero@ceatus.com>
2017-01-28 01:42:29 -0800

    added payments model

---
+++ db/migrate/20170128093956_create_payments.rb
@@ -0,0 +1,12 @@
+class CreatePayments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :payments do |t|
+      t.integer :user_id
+      t.integer :debt_id
+      t.integer :cents
+      t.datetime :paid
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110073531) do
+ActiveRecord::Schema.define(version: 20170128093956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20170110073531) do
     t.string   "type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "debt_id"
+    t.integer  "cents"
+    t.datetime "paid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
