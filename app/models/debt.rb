aea162f798b7b37a7ed48be72ced7684cc0dba3a
Mike Guerrero <mguerrero@ceatus.com>
2017-02-28 00:17:27 -0800

    fixed bill show page

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,11 @@
 class Debt < ApplicationRecord
+  belongs_to :user
+  belongs_to :apartment
+  has_many :payments
   attr_accessor :dollars
   validates :cents, numericality: { greater_than: 0 }
+
+  def total_paid
+    payments.sum(:cents)
+  end
 end
