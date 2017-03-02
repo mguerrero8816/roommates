3ed483c0cd8546b94629f4c762a4f30b694d7bc5
Mike Guerrero <mguerrero@ceatus.com>
2017-03-01 21:59:48 -0800

    removed bill simple route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # SIMPLE ROUTES
-  resources :bills
 
   # CUSTOM ROUTES
   resources :landing, only: [] do
