420cb68747135856a50aaa32cd9b562cc8af4382
Mike Guerrero <mguerrero@ceatus.com>
2017-03-05 13:30:21 -0800

    set method for standardizing date across application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def cents_to_dollars(cents)
     "$#{cents/100}.#{(cents%100).to_s.rjust(2, '0')}"
   end
+
+  # takes: date
+  # returns: string
+  # standard date format across application
+  def standard_date(date)
+    date ? date.strftime('%B %d, %Y') : nil
+  end
 end
