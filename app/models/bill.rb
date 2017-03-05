1f5377fa1d1207d9343c0c23885df0c53888a620
Mike Guerrero <mguerrero@ceatus.com>
2017-03-05 14:18:18 -0800

    only bills splittable

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,16 @@
 class Bill < Debt
   validates_presence_of :apartment_id
   belongs_to :apartment
+
+  def split_credit
+    split_factor = apartment.tenants.count
+    debt_owner = user.full_name
+    last_payment = active_payments.last
+    credit_name = "Payment to #{debt_owner} for #{name} fully paid on #{last_payment.paid}"
+    apartment.tenants.each do |user|
+      # skip the person who paid the debt
+      next if user_id == user.id
+      Credit.create(user_id: user.id, name: credit_name, pay_to_id: user_id, cents: cents/split_factor)
+    end
+  end
 end
