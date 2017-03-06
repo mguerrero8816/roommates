c757e19842d047e7a0392168529064ce7152706d
Mike Guerrero <mguerrero@ceatus.com>
2017-03-05 17:47:09 -0800

    set up different views for basic vs apartment bills

--- app/models/reporter/user_bills.rb
+++ app/models/reporter/user_bills.rb
@@ -4,6 +4,7 @@ module Reporter
       def report(user_id)
         bills = User.where(id: user_id)
         bills = join_bills(bills)
+        bills = join_apartments(bills)
         bills = join_bill_owners(bills)
         bills = add_bill_payments(bills)
         bills
@@ -15,6 +16,12 @@ module Reporter
                       bills.cents AS bill_cents,
                       bills.due AS bill_due')
              .joins("LEFT JOIN debts AS bills ON users.id = bills.user_id AND bills.type = 'Bill' ")
+      end
+
+      def join_apartments(bills)
+        bills.select('apartments.id AS apartment_id,
+                      apartments.name AS apartment_name')
+             .joins('LEFT JOIN apartments ON bills.apartment_id = apartments.id')
       end
     end
   end
