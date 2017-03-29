93062e445531054996f997ef571f86026f3423bc
Mike Guerrero <mguerrero@ceatus.com>
2017-03-29 12:04:16 -0700

    showing totals on main page

--- app/models/reporter/roommates.rb
+++ app/models/reporter/roommates.rb
@@ -24,26 +24,22 @@ module Reporter
              .joins('LEFT JOIN users AS roommates ON tenants.user_id = roommates.id')
       end
 
-      def roommate_credit_query(user_id, roommate_id)
-        credits = Credit.select('debts.*').where('(pay_to_id = ? AND user_id = ?) OR (pay_to_id = ? AND user_id = ?)', user_id, roommate_id, roommate_id, user_id)
-        credits = join_credit_apartments(credits)
-        credits
-      end
-
       def build_roommates_table(roommates, user_id)
         table = []
         roommates_added = []
         roommates.each do |roommate|
           next if roommates_added.include?(roommate.roommate_id)
           next if roommate.roommate_id == user_id
-          table << { user: roommate, credits: roommate_credit_query(user_id, roommate.roommate_id) }
+          roommate_data = { user: roommate, credits: roommate_credit_query(user_id, roommate.roommate_id) }
+          roommate_data[:total_balance] = calculate_balance(roommate_data[:credits], user_id)
+          table << roommate_data
           roommates_added << roommate.roommate_id
         end
         table
       end
 
-      def query_credits(pay_to_id, user_id)
-        credits = Credit.select('debts.*').where(pay_to_id: pay_to_id, user_id: user_id)
+      def roommate_credit_query(user_id, roommate_id)
+        credits = Credit.select('debts.*').where('(pay_to_id = ? AND user_id = ?) OR (pay_to_id = ? AND user_id = ?)', user_id, roommate_id, roommate_id, user_id)
         credits = join_credit_apartments(credits)
         credits
       end
@@ -51,6 +47,18 @@ module Reporter
       def join_credit_apartments(credits)
         credits.select('apartments.name AS apartment_name')
                .joins('LEFT JOIN apartments ON debts.apartment_id = apartments.id')
+      end
+
+      def calculate_balance(credits, user_id)
+        total_lent = 0
+        credits.select{ |credit| credit.pay_to_id == user_id }.each do |credit|
+          total_lent += credit.cents
+        end
+        total_owed = 0
+        credits.reject{ |credit| credit.pay_to_id == user_id }.each do |credit|
+          total_owed += credit.cents
+        end
+        total_lent - total_owed
       end
     end
   end
