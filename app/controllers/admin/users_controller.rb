1654fe7d919ab973b684b902927d1aecf8bb0083
Mike Guerrero <mguerrero@ceatus.com>
2017-06-08 16:49:18 -0700

    admins can create new users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,7 +7,13 @@ class Admin::UsersController < AdminController
   end
 
   def create
-    # @user = User.new(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_index_path, notice: 'User was successfully created'
+    else
+      flash[:alert] = 'User was not created'
+      render :new
+    end
   end
 
   def edit
@@ -36,6 +42,6 @@ class Admin::UsersController < AdminController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
