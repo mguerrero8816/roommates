0207ed45711e7ff5473bd5c9ace68b9005010de1
Mike Guerrero <mguerrero@ceatus.com>
2017-06-08 12:59:13 -0700

    created routes views and controller for admin users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # SIMPLE RESOURCES
   resources :credits, only: [:index]
   resources :debts,   only: [:index]
+  resources :admin,  only: [:index]
 
   # CUSTOM RESOURCES
   resources :apartments, only: [] do
@@ -25,12 +26,13 @@ Rails.application.routes.draw do
     get :current, on: :collection
   end
 
-  resources :admin, only: [:index] do
-
-  end
 
 
   # NAMESPACE RESOURCES
+  namespace :admin do
+    resources :users, except: [:index]
+    resources :roles
+  end
 
   # COMPLEX RESOURCES
   resources :bills, except: [:destroy] do
