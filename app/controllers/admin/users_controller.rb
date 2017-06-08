c19569ff763d31546907f0db8ea85b1b73091219
Mike Guerrero <mguerrero@ceatus.com>
2017-06-08 15:12:45 -0700

    able to delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,17 +3,24 @@ class Admin::UsersController < AdminController
   end
 
   def new
+    @user = User.new
   end
 
   def create
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+    else
+    end
+    redirect_to admin_index_path
   end
 end
